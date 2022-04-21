6d44867f6fe88dca5bb5fe1b1976469ae1a808c4
Paige Wright <paigeawright@outlook.com>
2022-04-21 23:50:31 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
 }
-        
+function read_value()
+{
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value + '%';
+
+}
+function validate_number()
+{
+    amount = document.getElementById("principal").value;
+
+    if (amount <= 0) {
+        alert('Enter a positive number')
+        document.getElementById("principal").focus()
+        return false
+    }
+    
+    return true
+}
